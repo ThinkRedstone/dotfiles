96716c15b25b85ea6d1b83473617e8453397c346
Ittai Dafner <dafner@epsio.io>
2024-07-21 23:31:15 +0300

    Add multi-monitor support to Hyprland

--- hypr/launcher.sh
+++ hypr/launcher.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-dmenu-wl_run -sf '#ff0000' -sb '#111111'
+monitor=$(hyprctl monitors | grep "focused: yes" -B 11 | head -n1 | cut -d" " -f 2)
+dmenu-wl_run -sf '#ff0000' -sb '#111111' -m "$monitor"
