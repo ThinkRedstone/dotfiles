214913139d326f1f909de69c7e1fd54b16895f23
ThinkRedstone <ittai11@gmail.com>
2024-11-10 23:33:49 +0200

    Finish theme engine

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     test -e venv/bin/activate.fish; and source venv/bin/activate.fish
+    test -e ~/.config/theme/fish_colors.fish; and source ~/.config/theme/fish_colors.fish
 end
---
+++ tress_theme/fish_colors.fish
@@ -0,0 +1,36 @@
+set -g fish_color_normal normal
+set -g fish_color_command 4B7939
+set -g fish_color_quote b2ab99
+set -g fish_color_redirection 00afff
+set -g fish_color_end 009900
+set -g fish_color_error ff0000
+set -g fish_color_param 00afff
+set -g fish_color_comment 990000
+set -g fish_color_selection white --bold --background=brblack
+set -g fish_color_search_match bryellow --background=brblack
+set -g fish_color_history_current --bold
+set -g fish_color_operator 00a6b2
+set -g fish_color_escape 00a6b2
+set -g fish_color_cwd CDB129
+set -g fish_color_cwd_root yellow
+set -g fish_color_valid_path --underline
+set -g fish_color_autosuggestion 555
+set -g fish_color_user 796600
+set -g fish_color_host normal
+set -g fish_color_cancel --reverse
+set -g fish_pager_color_background 
+set -g fish_pager_color_prefix white --bold --underline
+set -g fish_pager_color_progress brwhite --background=cyan
+set -g fish_pager_color_completion 
+set -g fish_pager_color_description B3A06D
+set -g fish_pager_color_selected_background --reverse
+set -g fish_pager_color_selected_prefix 
+set -g fish_pager_color_selected_completion 
+set -g fish_pager_color_selected_description 
+set -g fish_color_option 
+set -g fish_pager_color_secondary_completion 
+set -g fish_pager_color_secondary_description 
+set -g fish_color_keyword 
+set -g fish_color_host_remote 
+set -g fish_pager_color_secondary_prefix 
+set -g fish_pager_color_secondary_background 
