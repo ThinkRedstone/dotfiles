d6bf04d1cfc24532dd0287845728efd11eecf81d
Ittai Dafner <dafner@epsio.io>
2024-11-17 04:09:54 -0700

    Add script to switch between themes

---
+++ hypr/switch_theme.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+THEME_DIR=$(dirname ${BASH_SOURCE})/../themes
+THEME=$(realpath $THEME_DIR/${1}/)
+
+if [[ ! -d $THEME ]]; then
+  echo "No such theme: $1"
+  exit 1
+fi
+
+# change theme
+rm ~/.config/theme
+ln -s "$THEME" ~/.config/theme
+
+# reload wallpaper
+hyprctl hyprpaper unload ~/.config/theme/wallpaper.png
+hyprctl hyprpaper preload  ~/.config/theme/wallpaper.png
+hyprctl hyprpaper wallpaper ,~/.config/theme/wallpaper.png
+
+# reload kitty conf
+pkill -USR1 -f kitty
+
+killall waybar; waybar >/dev/null 2>&1 & disown
